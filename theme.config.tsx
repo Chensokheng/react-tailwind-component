ab56eb0b8e77cb567820227358d6a7c40459c6aa
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2023-04-02 16:49:01 +0700

    chore: update page seo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "./components/Logo";
 import { DocsThemeConfig } from "nextra-theme-docs";
+import { useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
 	logo: <Logo />,
@@ -28,24 +29,35 @@ const config: DocsThemeConfig = {
 	feedback: {
 		content: null,
 	},
-	head: (
-		<>
-			<meta
-				name="viewport"
-				content="width=device-width, initial-scale=1.0"
-			/>
-			<meta property="og:title" content="Daily Componets" />
-			<meta
-				property="og:description"
-				content="an open source React/Next.js Tailwind UI components and templates to bootstrap your new apps, projects or landing sites"
-			/>
-		</>
-	),
+	head: () => {
+		const { frontMatter } = useConfig();
+
+		return (
+			<>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+				<meta
+					property="og:title"
+					content={frontMatter.title || "Daily ReactUIs"}
+				/>
+				<meta
+					property="og:description"
+					content={
+						frontMatter.description ||
+						"Welcome to Daily Components ! This is open source React/Next.js Tailwind UI components and templates to bootstrap your new apps, projects or landing sites."
+					}
+				/>
+			</>
+		);
+	},
 	useNextSeoProps() {
 		return {
 			titleTemplate: "%s – Daily",
 		};
 	},
+	faviconGlyph: "✒️",
 };
 
 export default config;
